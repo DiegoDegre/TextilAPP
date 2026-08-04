527a5202f0905735f7d2e605936cb4d22a1ca9e8
DiegoDegre <degregoriodiego@live.com>
2026-08-04 17:37:24 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,4 @@
-const SHEETS_CONFIG = {
-  spreadsheetId: "1MzjHfGX3tIj2v0QcO4UJKoxvkov7F9keGHiGRzbn1k4",
-  clientEmail: "textilapp-service@textilapp-498300.iam.gserviceaccount.com",
-  privateKey: "",
-  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-  sheets: {
-    general: "GENERAL",
-    telas: "TELAS (INSUMO)",
-    servicios: "SERVICIOS REQUERIDOS  ADICIONAL"
-  }
+const APP_CONFIG = {
+    apiUrl: "https://script.google.com/macros/s/AKfycbzOt-sCePBXVT2OZl-SafoPQU7cpPx3MXc080m5EPmw5eIXNlxjAvpKNOIfWirPg_g/exec",
+    token: "LALOCURAESUNPLACERQUESOLOELLOCOCONOCEESOUNCHETOLODESCONOCE04082026"
 };
